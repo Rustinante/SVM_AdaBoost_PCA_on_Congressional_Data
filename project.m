593a7248e4a9d78483df9c8c38ee863d7c3c970e
chloedrazen <chloedrazen@ucla.edu>
2016-05-21 17:04:40 -0700

    Update project.m
    Added Adaboost classification on PCA of data

--- project.m
+++ project.m
@@ -1,3 +1,9 @@
+source boosteval.m;
+source boostlearn.m;
+source weaklearn.m;
+source weakeval.m;
+
+
 % -1 = Republican
 %  1 = Democrat
 %  1 = Yes
@@ -31,6 +37,16 @@ w = [0.0762;
    -0.0269 ];
 b = 0.2675;
 
+
+x_axis = X * w + b;
+figure
+
+plot(t, x_axis, 'b')
+title('Classification')
+xlabel('t, the correct labels')
+ylabel('X*w +b')
+
+
 % Most and least distinguishable vote for classification
 w_square = w.^2;
 [max_val_vote max_ind_vote] = max(w_square); %4=physician_fee_freeze
@@ -40,13 +56,6 @@ w_square = w.^2;
 
 [min_val_person, min_index_person] = min(x_axis); %303 --> Most Republican
 [max_val_person, max_index_person] = max(x_axis); %106 --> Most Democratic
-
-x_axis = X * w + b;
-figure
-plot(t, x_axis, 'b*')
-title('Classification')
-xlabel('t, the correct labels')
-ylabel('X*w +b')
 
 
 R_indices = find(t==-1);
@@ -129,10 +138,83 @@ end %for_loop
  
  figure
  hold on
- scatter(x_axis(R_indices), y_axis(R_indices), 'r*')
- scatter(x_axis(D_indices), y_axis(D_indices), 'b*')
+
+scatter(x_axis(R_indices), y_axis(R_indices), 'r')
+ scatter(x_axis(D_indices), y_axis(D_indices), 'b')
  title('PCA')
  xlabel('PC1')
  ylabel('PC2')
  hold off
+
+
+ %% ================== ADABoost Algorithm ================== %%
+
+%Need to first make data two dimensional, so squishing it down onto 2 PCs
+%scatter(x_axis(R_indices), y_axis(R_indices), 'r')
+%scatter(x_axis(D_indices), y_axis(D_indices), 'b')
+
+dems = zeros(2,size(D_indices, 1));
+dems(1, :) = x_axis(D_indices)';
+dems(2, :) = y_axis(D_indices)';
+
+repubs = zeros(2,size(R_indices, 1));
+repubs(1, :) = x_axis(R_indices)';
+repubs(2, :) = y_axis(R_indices)';
+%dems = X(D_indices, :);
+
+%repubs = X(R_indices, :);
+
+min_size = min(size(dems,2), size(repubs,2))
+dems = dems(:, 1:min_size);
+repubs = repubs(:, 1:min_size);
+
+X0 = dems;
+X1 = repubs;
+
+[params, weights] = boostlearn(X0, X1, 2);
+
+C5_0 = boosteval(X0, params, weights);
+C5_1 = boosteval(X1, params, weights);
+
+
+% iterate through c50, c51 and if its 1 plot it in one color, and it if 0 plot it in another color.
+scatter1 = zeros((min_size *2), 2);
+scatter2 = zeros((min_size *2), 2);
+scatter1_size = 1;
+scatter2_size = 1;
+for i = 1:min_size
+
+    if (C5_0(i) == 1)
+        scatter1(scatter1_size, 1) = (X0(1, i));
+        scatter1(scatter1_size, 2) = (X0(2, i));
+        scatter1_size = scatter1_size + 1;
+    else
+        scatter2(scatter2_size,1) = (X0(1,i));
+        scatter2(scatter2_size,2) = (X0(2,i));
+        scatter2_size = scatter2_size + 1;
+    endif
+end
+
+for i = 1:min_size
+
+    if (C5_1(i) == 1)
+        scatter1(scatter1_size, 1) = (X1(1, i));
+        scatter1(scatter1_size, 2) = (X1(2, i));
+        scatter1_size = scatter1_size + 1;
+    else
+        scatter2(scatter2_size,1) = (X1(1,i));
+        scatter2(scatter2_size,2) = (X1(2,i));
+        scatter2_size = scatter2_size + 1;
+    endif
+end
+
+figure
+
+hold on
+scatter(scatter1(:,1), scatter1(:,2), 'b')
+scatter(scatter2(:,1), scatter2(:,2), 'r')
+title("AdaBoost classification with 2 classes")
+hold off
+
+
  
